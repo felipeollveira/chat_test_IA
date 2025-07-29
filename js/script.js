26587f6a28d6a0e6fbbd42197b7757bb1e782a2d
f <110308764+felipeollveira@users.noreply.github.com>
2025-07-29 16:14:39 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@
     const input = document.getElementById("userInput");
     const sendButton = document.getElementById("sendButton");
 
-    function generateUUID() { // Public Domain/MIT
+    function generateUUID() { 
         var d = new Date().getTime();//Timestamp
-        var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+        var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = Math.random() * 16;//random number between 0 and 16
-            if(d > 0){//Use timestamp until depleted
+            var r = Math.random() * 16;
+            if(d > 0){
                 r = (d + r)%16 | 0;
                 d = Math.floor(d/16);
-            } else {//Use microseconds since page-load if supported
+            } else {
                 r = (d2 + r)%16 | 0;
                 d2 = Math.floor(d2/16);
             }
